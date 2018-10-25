dd81dd34440f2b8a296c1f3127ca2086c9032b65
Jeremiah <almostgone47@hotmail.com>
2018-10-25 13:27:41 +0800

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_24_071522) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: :cascade do |t|
     t.string "name"
     t.string "address"
